e95faee62deb6bcbeb5a140c0e3839484553dba8
Oliver Vinogradic <ovinog6@gmail.com>
2026-08-11 19:41:53 +0200

    Update bit-hex_calc.py

--- bit-hex_calc.py
+++ bit-hex_calc.py
@@ -1,18 +1,27 @@
 
 def userinput():
   print(f"Hey,choose some numbers and calculate the Bin or Hex\n")
+
   us_input = int(input("Pick a Number\n"))
-  u_choice = int(input(f"alright now choose 1.for Bin and 2. For Hex\n"))
-  if u_choice > 2 or u_choice < 1 :
+  us_choice = int(input(f"alright now choose 1.for Bin and 2. For Hex\n"))
+
+  if us_choice > 2 or us_choice < 1 :
      print("The Choice is not listed...")
+     return userinput()
   else:
-     return us_input
+     return us_input,us_choice
 
 
 
-def calculator(u_input):
+def calculator(u_input,u_choice):
+  if u_choice == 1:
+   bin_text = bin(u_input)[2:].zfill(8)
+   return bin_text
+  else:
+   hex_text = hex(u_input)[2:].upper().zfill(2)
+   return hex_text
 
- bin_text = bin(u_input)[2:].zfill(8)
+
 
 
 
@@ -20,8 +29,10 @@ def calculator(u_input):
 
 def main():
     print("Calc Starts")
-    result = calculator(us_input)
+    u_result,u_choice = userinput()
+    result = calculator(u_result,u_choice)
+    print(result)
 
 
 if __name__ == "__main__":
-    main()
+     main()
