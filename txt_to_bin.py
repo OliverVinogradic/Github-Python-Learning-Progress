6342b54ff1887b6edcaec1788da70d55d30effa9
Oliver Vinogradic <ovinog6@gmail.com>
2026-08-18 21:28:17 +0200

    Update txt_to_bin.py

--- txt_to_bin.py
+++ txt_to_bin.py
@@ -1,29 +1,33 @@
 
-def main():
-    Bin_list = []
-    number_count = 0
-    dev_num = number_count // 8
-    i = 1
-    try:
-        with open("bin.txt","r") as data_bin:
-            number_count = 0
-            Bin_octet = data_bin.read()
-            for bin in Bin_octet:
-                 if bin.isdigit():
-                  number_count +=1
-                 else:
-                     print("Unknow Error")
-
-        dev_num = number_count // 8
-        for i in range(dev_num):
-         Bin_list.append(Bin_octet[:8])
-
-
-    except FileNotFoundError:
-        print("File not Found")
+#Opening the bit text file and transfering its data
+def bin_txt_trans():
+        Bin_list = []
+        try:
+            with open("bin.txt","r") as data_bin:
+                number_count = 0
+                Bin_octet = data_bin.read()
+                for bin in Bin_octet:
+                     if bin.isdigit():
+                      number_count +=1
+                     else:
+                         print("Unknow Error")
     
-    return Bin_list
+            dev_num = number_count // 8
+            for i in range(dev_num):
+             Bin_list.append(Bin_octet[:8])
+    
+    
+        except FileNotFoundError:
+            print("File not Found")
+        
+        return Bin_list
+    
+    
 
+def main():
+    print("Main function start")
+    
+   
 
 if __name__ == "__main__":
     main()
