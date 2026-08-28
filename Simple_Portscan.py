1fe3830e93ba041b953beb90c3a6d2ca5dee25c1
Oliver Vinogradic <ovinog6@gmail.com>
2026-08-28 17:35:29 +0200

    Updated the Portscanner
    Updated the Simple Portscanners to send out http requests to receive the banner

--- Simple_Portscan.py
+++ Simple_Portscan.py
@@ -13,7 +13,14 @@ try:
     else:
         print(f"[-] Connection to {s_target} failed!")
 
+    http_request = "HEAD / HTTP/1.1\r\nHost: scanme.nmap.org\r\n\r\n"
+    s.send(http_request.encode('utf-8'))
 
-
+    banner = s.recv(1024).decode('utf-8', errors='ignore')
+    
+    print("\n--- Recieved BANNER ---")
+    print(banner)
+    print("--------------------------")
+    
 except Exception as e:
     print(f"[-] Error {e}")
